e0313fd2a7e0a49737f32a0d0ac3bbb4d9af9eff
44Chick <to44guchi@gmail.com>
2022-12-03 09:30:31 +0900

    hotfix_0_1

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,7 +1,18 @@
 import { createStore } from "redux";
 import { combineReducers } from "redux";
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers(
+  [{
+    id : 1,
+    title : 'test title',
+    detail : 'test detail. this is sentence.'
+  },{
+    id : 1,
+    title : 'test title',
+    detail : 'test detail. this is sentence.'
+  },
+])
+;
 const store = createStore(rootReducer);
 
 export default store;
